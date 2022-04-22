64925e1686cc92f319a73cc729f9935bbbdd9b2e
liuzhuo <you@exa>
2022-04-22 18:03:55 +0800

    elastis java客户端

---
+++ elastic-lzy/src/test/java/com/demo/elastic/ESTest.java
@@ -0,0 +1,18 @@
+package com.demo.elastic;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lzy
+ * Date: 2022/4/20
+ * Time: 14:17
+ * Description: No Description
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ESTest {
+
+}
