1ebc825b0b449e294fe20ed7b2eaa5260f80e692
lzy <1204375052@qq.com>
2022-05-11 22:24:25 +0800

    线程池原理

--- stream/src/main/java/com/demo/Test.java
+++ stream/src/main/java/com/demo/Test.java
@@ -1,5 +1,10 @@
 package com.demo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: lzy
@@ -9,6 +14,21 @@ package com.demo;
  */
 public class Test {
     public static void main(String[] args) {
+        short s1 = 1;
+        s1 = (short) (s1 +1);
+        s1 += 1; //有隐含的强制类型转换。
 
+        String str = new String();
+
+        List<String> list = new ArrayList<>();
+        list. add("x");
+        //只读集合
+        Collection<String> clist = Collections. unmodifiableCollection(list);
+        clist.add("s");//报错
+
+        //两种插入是有区别的 非顺序插入和删除 是会copy的
+        list.add("s");
+        list.add(1,"s");
+        list.remove(1);
     }
 }
